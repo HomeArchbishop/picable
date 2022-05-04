ae7d82ec2a9c704b6210d04bcf0898546c917f0f
拆家大主教 <7687020+HomeArchbishop@user.noreply.gitee.com>
2022-05-04 21:32:02 +0800

    refactor: rewrite the url resolver by new URL

--- src/assets/utils/formatImgUrl.js
+++ src/assets/utils/formatImgUrl.js
@@ -3,6 +3,6 @@ import { sanitizeUrl } from '@braintree/sanitize-url'
 export const formatImgUrl = function (fileServer = '', path = '') {
   const unsafeResult = fileServer.slice(0, 1) === '@'
     ? path
-    : `https://storage1.picacomic.com/static/${path}`
+    : new URL(path, 'https://storage1.picacomic.com/static/').toString()
   return fileServer.slice(0, 1) === '@' ? unsafeResult : sanitizeUrl(unsafeResult)
 }
